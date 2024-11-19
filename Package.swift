bb983a0e19a5dcf2a9c2d77d852f356f2935df1a
RickStreet <rick_1@me.com>
2024-11-19 12:48:58 -0800

    Updated package dependencies

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/RickStreet/DialogKit.git", from: "2.0.0"),
-        .package(url: "https://github.com/RickStreet/DoubleKit.git", from: "1.0.7")
+        .package(url: "https://github.com/RickStreet/DialogKit.git", from: "2.0.1"),
+        .package(url: "https://github.com/RickStreet/DoubleKit.git", from: "1.0.8")
 
     ],
     targets: [
